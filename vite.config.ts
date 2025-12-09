59af2299952e89db3955e1e0b22ce2c1e302d324
jaaaaavier <javipiagui13@gmail.com>
2025-12-09 16:23:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'; 
+import { defineConfig } from 'vite'; 
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
-export default defineConfig(({ mode }) => {
-  
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: '/',
-    plugins: [react()],
-    css: {
-      postcss: {
-        plugins: [tailwindcss],
-      },
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
-    preview: {
-      port: 3000,
-      open: true,
-    },
-    server: {
-      port: 3000,
-      open: true,
-      proxy: {
-        '/api': {
-          target: env.AI_SERVER_URL, 
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  };
+  },
+  preview: {
+    port: 3000,
+    open: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
