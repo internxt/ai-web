da64a729c63ebea01e19b7f791dc42c737ea20e2
jaaaaavier <javipiagui13@gmail.com>
2025-12-09 16:55:48 +0100

    feat: update 403

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'https://backendai.internxt.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
   },
 });
